e2b02561eaa9d3786431252eb1f7e254dd5a5a8c
intanbalqis <intanpolindra@gmail.com>
2025-10-01 15:03:44 +0700

    menambahkan file arrayexample.java di folder arraycollection

---
+++ arraycollection.java/ArrayExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi array
+        String[] buah = {"Apel", "Mangga", "Jeruk"};
+
+        // Menampilkan isi array
+        System.out.println("Daftar Buah:");
+        for (int i = 0; i < buah.length; i++) {
+            System.out.println(buah[i]);
+        }
+    }
+}
