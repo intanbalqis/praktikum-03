a6f12d9bab6acf59f726807b060783ef95a7baa2
intanbalqis <intanpolindra@gmail.com>
2025-10-03 23:47:57 +0700

    menambahkan file Import.java di folder package

---
+++ package/Import.java
@@ -0,0 +1,10 @@
+package package;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Import {
+    ArrayList<String> list = new ArrayList<>();
+    HashMap<Integer, String> map = new HashMap<>();
+    Scanner input = new Scanner(System.in);
+}
